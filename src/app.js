a30d4c1924a0950424da20593e85e36facdbf697
Ufuk Bağcı <ufukbagci@aof.anadolu.edu.tr>
2017-06-21 02:54:00 +0300

    Proje Çalıştı

--- src/app.js
+++ src/app.js
@@ -1,12 +1,55 @@
+/*window.addEventListener('load', () => {
+
+    window.vue  = new Vue({
+    el : "#app",
+    name : "JsonPage",
+    post:[],
+    data : {
+        message : "Merhaba",
+
+        giris : true,
+        username : "Ufuk"
+    },
+    created()   {
+
+        fetch("https://jsonplaceholder.typicode.com/posts")
+            .then((res) => {return res.json() })
+    .then((res) => {
+
+
+        })
+
+
+
+
+    } //Created Bitiş.
+})
+}); */
+
+
 window.addEventListener('load', () => {
+    window.vue = new Vue({
 
-window.vue = new Vue({
- el:'#app',
- data : {
-     giris : true,
-     message : "Merhaba",
-     username : 'Ufuk'
- }
+        el: "#app",
+        name: "JsonTest",
+        comments:[],
+        data: {
+            message: "Developer",
+            logged: true,
+            username:"Ufuk",
+            isLoading : true
+        },
+        created(){
+            fetch("src/data.json")
+                .then((res) => {return res.json()})
+                .then((res) => {
+                this.isLoading = false;
+                  this.comments = res.comments;
 
-})
-})
+                })
+        } //created bitiş.
+
+
+    })
+
+});
